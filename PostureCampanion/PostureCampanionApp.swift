070870d45d24d7d822fb4263ea4e2b7468f936fd
Riser17 <harshmaharajpura1998@gmail.com>
2026-06-20 22:50:50 +0530

    Initial Commit

---
+++ PostureCampanion/PostureCampanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureCampanionApp.swift
+//  PostureCampanion
+//
+//  Created by Harshvardhan Rathore on 20/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PostureCampanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PostureCampanionTests/PostureCampanionTests.swift
@@ -0,0 +1,19 @@
+//
+//  PostureCampanionTests.swift
+//  PostureCampanionTests
+//
+//  Created by Harshvardhan Rathore on 20/06/26.
+//
+
+import Testing
+@testable import PostureCampanion
+
+struct PostureCampanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
